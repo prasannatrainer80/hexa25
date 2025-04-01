852d877f0eb6d4bfaa853fe867d012c4d74b2c38
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2025-04-01 10:36:25 +0530

    sql maven

--- April_1/EmployShowMain.java
+++ April_1/EmployShowMain.java
@@ -2,6 +2,8 @@ package com.java.employ;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EmployShowMain {
@@ -10,7 +12,19 @@ public class EmployShowMain {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hexamarch", "root", "root");
-			System.out.println("Connected...");
+//			System.out.println("Connected...");
+			String cmd = "select * from Employ";
+			PreparedStatement pst = connection.prepareStatement(cmd);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				System.out.println("Employ No  " +rs.getInt("empno"));
+				System.out.println("Employ Name  " + rs.getString("Name"));
+				System.out.println("Gender  " +rs.getString("gender"));
+				System.out.println("Department  " +rs.getString("dept"));
+				System.out.println("Designation  " +rs.getString("desig"));
+				System.out.println("Salary  " +rs.getDouble("basic"));
+				System.out.println("---------------------------------------------------------");
+			}
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
